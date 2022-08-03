07ed3bedcd70c0405f5f3535210f31bb48973911
venkylad <venkateshkonukuladdu888@gmail.com>
2022-08-03 16:35:53 +0530

    step back

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import React, { useState } from 'react'
-import Logo from './images/logo.svg'
-import './App.css'
-import Step1 from './components/steps/Step1'
-import Step2 from './components/steps/step2'
-import Step3 from './components/steps/Step3'
-import Step4 from './components/steps/Step4'
-import Steps from './components/steps/Steps'
+import React, { useState } from 'react';
+import Logo from './images/logo.svg';
+import './App.css';
+import Step1 from './components/steps/Step1';
+import Step2 from './components/steps/step2';
+import Step3 from './components/steps/Step3';
+import Step4 from './components/steps/Step4';
+import Steps from './components/steps/Steps';
 
 const App = () => {
-
-  const [step, setStep] = useState(1)
+  const [step, setStep] = useState(1);
 
   const handleNextStep = () => {
-    setStep((prevState) => prevState + 1)
-  }
+    setStep((prevState) => prevState + 1);
+  };
+
+  const handleStepBack = (num) => {
+    if (num && num <= step) {
+      setStep(num);
+    }
+  };
 
   return (
     <div className='container'>
       <div className='logo-div'>
-        <img className='logo' src={Logo} alt="" />
+        <img className='logo' src={Logo} alt='' />
         Eden
       </div>
-      <Steps step={step} />
+      <Steps step={step} handleNextStep={handleStepBack} />
       <Step1 step={step} handleNextStep={handleNextStep} />
       <Step2 step={step} handleNextStep={handleNextStep} />
       <Step3 step={step} handleNextStep={handleNextStep} />
       <Step4 step={step} handleNextStep={handleNextStep} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/steps/Steps.js
+++ src/components/steps/Steps.js
@@ -1,16 +1,33 @@
 import React from 'react';
 
-const Steps = ({ step }) => {
+const Steps = ({ step, handleNextStep }) => {
   const progress = ['20%', '50%', '80%', '100%'];
 
   return (
     <div className='steps-container'>
       <div className='line' />
       <div className='progress-line' style={{ width: progress[step - 1] }} />
-      <div className='step-active'>1</div>
-      <div className={step > 1 ? 'step-active' : 'step'}>2</div>
-      <div className={step > 2 ? 'step-active' : 'step'}>3</div>
-      <div className={step > 3 ? 'step-active' : 'step'}>4</div>
+      <div className='step-active' onClick={() => handleNextStep(1)}>
+        1
+      </div>
+      <div
+        className={step > 1 ? 'step-active' : 'step'}
+        onClick={() => handleNextStep(2)}
+      >
+        2
+      </div>
+      <div
+        className={step > 2 ? 'step-active' : 'step'}
+        onClick={() => handleNextStep(3)}
+      >
+        3
+      </div>
+      <div
+        className={step > 3 ? 'step-active' : 'step'}
+        onClick={() => handleNextStep(4)}
+      >
+        4
+      </div>
     </div>
   );
 };
